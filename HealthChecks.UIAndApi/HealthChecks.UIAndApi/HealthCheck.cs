db6857b81393de21998bf1b80272392db045e1f1
mm-aneris <mm.anerishah@gmail.com>
2024-12-12 18:03:20 +0530

    Add Project Files.

---
+++ HealthChecks.UIAndApi/HealthChecks.UIAndApi/HealthCheck.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace HealthChecks.UIAndApi
+{
+    public static class HealthCheck
+    {
+        public static void ConfigureHealthChecks(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddHealthChecks()
+                .AddCheck<RemoteHealthCheck>("Remote endpoints Health Check", failureStatus: HealthStatus.Unhealthy)
+                .AddCheck<WeatherApiHealthCheck>($"Feedback Service Memory Check", failureStatus: HealthStatus.Unhealthy, tags: new[] { "Feedback Service" });
+
+            services.AddHealthChecksUI(opt =>
+            {
+                opt.SetEvaluationTimeInSeconds(10); //time in seconds between checks    
+                opt.MaximumHistoryEntriesPerEndpoint(60); //maximum history of checks    
+                opt.SetApiMaxActiveRequests(1); //api requests concurrency    
+                opt.AddHealthCheckEndpoint("feedback api", "/api/health"); //map health check api    
+
+            }).AddInMemoryStorage();
+        }
+    }
+}
---
+++ HealthChecks.UIAndApi/HealthChecks.UIAndApi/Program.cs
@@ -0,0 +1,45 @@
+using HealthChecks.UI.Client;
+using HealthChecks.UIAndApi;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Congiguring Health Check
+builder.Services.ConfigureHealthChecks(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//HealthCheck Middleware
+app.MapHealthChecks("/api/health", new HealthCheckOptions()
+{
+    Predicate = _ => true,
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
+app.UseHealthChecksUI(options =>
+{
+    options.UIPath = "/healthcheck-ui";
+    //options.AddCustomStylesheet("./HealthCheck/Custom.css");
+});
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
